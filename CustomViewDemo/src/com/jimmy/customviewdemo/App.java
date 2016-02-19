554b6682e978020d9fec088979bc7fed37e03d1b
jinguochong@gmail.com <jinguochong@gmail.com>
2016-02-19 19:08:31 +0800

    流式布局完成

---
+++ CustomViewDemo/src/com/jimmy/customviewdemo/App.java
@@ -0,0 +1,5 @@
+package com.jimmy.customviewdemo;
+
+public class App {
+
+}
---
+++ CustomViewDemo/src/com/jimmy/customviewdemo/ui/MainActivity.java
@@ -0,0 +1,41 @@
+package com.jimmy.customviewdemo.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.jimmy.customviewdemo.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.btn_large_img).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, LargeImgAty.class);
+				startActivity(intent);
+			}
+		});
+		
+		findViewById(R.id.btn_measure_once).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, MeasureOnceAty.class);
+				startActivity(intent);
+			}
+		});
+		
+		findViewById(R.id.btn_flow_view).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, FlowViewAty.class);
+				startActivity(intent);
+			}
+		});
+	}
+}
